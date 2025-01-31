a5d5f994d61261b525c150a912d2aebd5df36e5c
陈洪刚 <mesfet@126.com>
2025-01-31 23:29:18 +0800

    feat: 添加DFS存储后端实现

---
+++ src/storage/storage_backend_dfs.c
@@ -0,0 +1,5 @@
+/*
+ * storage_backend_dfs.c - Implementation of DFS storage backend
+ *
+ * This file provides DFS storage backend functionality for Libvirt.
+ */
---
+++ src/storage/storage_backend_dfs.h
@@ -0,0 +1,14 @@
+#ifndef STORAGE_BACKEND_DFS_H
+#define STORAGE_BACKEND_DFS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Add your declarations here
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // STORAGE_BACKEND_DFS_H
